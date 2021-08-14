b99b5d5c57da917149cb89619c874e1cec963a25
y.maruyama <y.maruyama@centos1.localdomain>
2021-08-14 17:58:02 +0900

    20210814

---
+++ srv.py
@@ -0,0 +1,177 @@
+import os
+import numpy as np
+import cv2
+import argparse
+from flask import Flask,Response,request,jsonify,session
+from dotenv import load_dotenv
+import json
+#import urllib.parse
+import urllib.request
+import base64
+from datetime import timedelta
+import mitsuba.imcut as imcut
+
+
+predicturl = None
+bg = None
+diff_thr=2000
+cutsize=(200,150)
+allsize=(640,480)
+
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+app.secret_key = "hogefuga"
+
+
+def init_session() :
+
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=5)
+    session.modified = True
+
+@app.route("/detect",methods=["POST"])
+def detect():
+
+    response = None
+
+    try:
+        if "islogin" not in session : raise Exception
+
+        _bytes = np.frombuffer(request.data, np.uint8)
+
+        frame = cv2.imdecode(_bytes, flags=cv2.IMREAD_COLOR)
+
+        detec,frame = framediff(frame)
+
+        _,jpeg = cv2.imencode('.jpg', frame)
+
+        #imgstr = jpeg.tostring()
+        imgstr = base64.b64encode(jpeg.tostring()).decode("utf-8")
+
+        res={"res":detec,"img":imgstr}
+
+        response = jsonify(res)
+        code=200
+        #response.headers[""] = ""
+
+    except :
+        code=500
+    finally :
+        return response,code
+#    return Response(jpeg.tobytes(),mimetype="image/jpeg")
+
+
+
+
+@app.route("/bgrenew",methods=["POST"])
+def bgrenew():
+    global bg
+
+    if "islogin" not in session :
+        init_session()
+        session["islogin"]="1"
+
+    _bytes = np.frombuffer(request.data, np.uint8)
+
+    frame = cv2.imdecode(_bytes, flags=cv2.IMREAD_COLOR)
+
+    bg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    res={"res":True}
+
+    return jsonify(res)
+
+
+
+def framediff(frame):
+#    global bg
+
+    if bg is None :
+        return None
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    frame_diff = cv2.absdiff(bg,gray)
+
+    _,frame_diff = cv2.threshold(frame_diff,50,255, cv2.THRESH_BINARY)
+    frame_diff = cv2.medianBlur(frame_diff, 5)
+
+    diff_point=cv2.countNonZero(frame_diff)
+
+    det1 = 0
+    det2 = 0
+
+    if diff_thr < diff_point:
+        contrs,hierarchy = cv2.findContours(frame_diff,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+        for pt in contrs:
+            mu = cv2.moments(pt)
+
+            if(mu["m00"]>2000):
+                cx=int(mu["m10"]/mu["m00"])
+                cy=int(mu["m01"]/mu["m00"])
+
+                cutwx,cutwy = imcut.adjust_size(mu["m00"],0.8,cutsize)
+
+                xx = imcut.cut_over(cx,cutwx,allsize[0])
+                yy = imcut.cut_over(cy,cutwy,allsize[1])
+
+                expimg=frame[yy[0]:yy[1],xx[0]:xx[1]]
+                #expimg = cv2.resize(expimg,cutsize)
+
+                color=(0, 255, 0)
+
+                if predict(expimg)>0.7 :
+                    det1 += 1
+                    color=(255, 0, 0)
+                else :
+                    det2 += 1
+
+                cv2.rectangle(frame,(xx[0],yy[0]),(xx[1],yy[1]),color, 1)
+
+    detec = -1
+
+    if det1>0 : detec = 1
+    elif det2>0 : detec = 0
+
+    return detec,frame
+
+
+def predict(frame):
+
+    #frame = cv2.resize(frame,(64,48))
+    _,jpeg= cv2.imencode(".jpg", frame)
+
+    req = urllib.request.Request(
+        predicturl,
+        jpeg.tobytes(),
+        method="POST",
+        headers={"Content-Type": "application/octet-stream"},
+    )
+
+    response = urllib.request.urlopen(req)
+    json_str = response.read()
+    response.close()
+
+    j = json.loads(json_str)
+
+    return j["prob"]
+
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-p","--port",type=int,default=5000)
+
+    args = parser.parse_args()
+
+    myport=int(args.port)
+
+    load_dotenv()
+
+    predicturl=os.getenv("PREDICTURL")
+
+    app.run(host='0.0.0.0', debug=False,threaded=True,port=myport)
