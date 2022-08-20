dde3aa3747dde739f0903f6156f7141f6d5af398
y.maruyama <y.maruyama@centos1.localdomain>
2022-08-20 21:52:43 +0900

    20220820-3

--- srv.py
+++ srv.py
@@ -19,9 +19,9 @@ import mitsuba.imcut as imcut
 predicturl = None
 savepath = None
 bg = None
-diff_thr = 2000
-cutsize = 200
-allsize = 640
+diff_thr = None
+cutsize = None
+allsize = None
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
